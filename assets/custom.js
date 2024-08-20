3f1c6554108a2510bda7f62f91f6be64d07a5a72
abdul gufran <abdulgufran@gmail.com>
2024-08-20 11:12:55 +0600

    first commit

---
+++ assets/custom.js
@@ -0,0 +1,43 @@
+
+
+function ourDigitalClock() {
+
+    let hoursElement = document.getElementById('hours');
+    let minutesElement = document.getElementById('minutes');
+    let secondsElement = document.getElementById('seconds');
+    let ampmElement = document.getElementById('ampm');
+    
+    
+    // console.log(hoursElement);
+    // console.log(minutesElement);
+    // console.log(secondsElement);
+    // console.log(ampmElement);
+    
+    
+
+    let now = new Date();
+    
+    
+    
+    let hoursNumber = (now.getHours()) > 12 ? (now.getHours()) - 12 : (now.getHours());
+    let minutesNumber = now.getMinutes();
+    let secondsNumber = now.getSeconds();
+    
+    let ampmString = (now.getHours()) > 12 ? 'PM' : 'AM';
+
+
+
+
+    hoursElement.innerHTML = hoursNumber < 10 ? '0' + hoursNumber : hoursNumber;
+    minutesElement.innerHTML = minutesNumber < 10 ? '0' + minutesNumber : minutesNumber;
+    secondsElement.innerHTML = secondsNumber < 10 ? '0' + secondsNumber : secondsNumber;
+    ampmElement.innerHTML = ampmString;
+
+}
+
+
+setInterval(ourDigitalClock, 1000);
+
+
+
+
